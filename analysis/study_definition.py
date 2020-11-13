3ba848721a70b037427585bb48795d54444deef0
wjchulme <25637345+wjchulme@users.noreply.github.com>
2020-11-13 13:44:46 +0000

    remove links to issues

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -24,8 +24,7 @@ study = StudyDefinition(
     population=patients.registered_with_one_practice_between(
         "2019-02-01", "2020-02-01"
     ),
-    # The rest of the lines define the covariates with associated GitHub issues
-    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/33
+
     age=patients.age_as_of(
         "2020-02-01",
         return_expectations={
@@ -33,14 +32,14 @@ study = StudyDefinition(
             "int": {"distribution": "population_ages"},
         },
     ),
-    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/46
+
     sex=patients.sex(
         return_expectations={
             "rate": "universal",
             "category": {"ratios": {"M": 0.49, "F": 0.51}},
         }
     ),
-    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/7
+
     chronic_cardiac_disease=patients.with_these_clinical_events(
         chronic_cardiac_disease_codes,
         returning="date",
@@ -48,7 +47,7 @@ study = StudyDefinition(
         include_month=True,
         return_expectations={"incidence": 0.2},
     ),
-    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/12
+
     chronic_liver_disease=patients.with_these_clinical_events(
         chronic_liver_disease_codes,
         returning="date",
@@ -59,7 +58,7 @@ study = StudyDefinition(
             "date": {"earliest": "1950-01-01", "latest": "today"},
         },
     ),
-    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/10
+
     bmi=patients.most_recent_bmi(
         on_or_after="2010-02-01",
         minimum_age_at_measurement=16,
@@ -70,7 +69,7 @@ study = StudyDefinition(
             "float": {"distribution": "normal", "mean": 35, "stddev": 10},
         },
     ),
-    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/35
+
     bp_sys=patients.mean_recorded_value(
         systolic_blood_pressure_codes,
         on_most_recent_day_of_measurement=True,
@@ -82,6 +81,7 @@ study = StudyDefinition(
             "float": {"distribution": "normal", "mean": 80, "stddev": 10},
         },
     ),
+
     bp_dias=patients.mean_recorded_value(
         diastolic_blood_pressure_codes,
         on_most_recent_day_of_measurement=True,
@@ -93,7 +93,7 @@ study = StudyDefinition(
             "float": {"distribution": "normal", "mean": 120, "stddev": 10},
         },
     ),
-    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/54
+
     stp=patients.registered_practice_as_of(
         "2020-02-01",
         returning="stp_code",
@@ -102,6 +102,7 @@ study = StudyDefinition(
             "category": {"ratios": {"STP1": 0.5, "STP2": 0.5}},
         },
     ),
+
     msoa=patients.registered_practice_as_of(
         "2020-02-01",
         returning="msoa_code",
@@ -110,7 +111,7 @@ study = StudyDefinition(
             "category": {"ratios": {"MSOA1": 0.5, "MSOA2": 0.5}},
         },
     ),
-    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/52
+
     imd=patients.address_as_of(
         "2020-02-01",
         returning="index_of_multiple_deprivation",
@@ -120,6 +121,7 @@ study = StudyDefinition(
             "category": {"ratios": {"100": 0.1, "200": 0.2, "300": 0.7}},
         },
     ),
+
     rural_urban=patients.address_as_of(
         "2020-02-01",
         returning="rural_urban_classification",
@@ -128,6 +130,7 @@ study = StudyDefinition(
             "category": {"ratios": {"rural": 0.1, "urban": 0.9}},
         },
     ),
+
     recent_salbutamol_count=patients.with_these_medications(
         salbutamol_codes,
         between=["2018-02-01", "2020-02-01"],
